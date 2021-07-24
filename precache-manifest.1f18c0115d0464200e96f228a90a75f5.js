c4c5c95fa5d607f2fa113ebc4469461a136ae77b
sergei-trofimov <feast.st@gmail.com>
2021-07-24 18:08:40 +0300

    Updates

---
+++ precache-manifest.1f18c0115d0464200e96f228a90a75f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6dff9e9e010bc3f30a11990276aa4b9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c4e1cfc3ca9c72a5d617",
+    "url": "./static/css/main.13490b1f.chunk.css"
+  },
+  {
+    "revision": "2ee7e75c1ec5fd868276",
+    "url": "./static/js/2.136d2ae0.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.136d2ae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4e1cfc3ca9c72a5d617",
+    "url": "./static/js/main.168cb0e3.chunk.js"
+  },
+  {
+    "revision": "cfec1821169abfb78760",
+    "url": "./static/js/runtime-main.182821f8.js"
+  },
+  {
+    "revision": "e687fa09e2d9741dd09bdd1c0766261d",
+    "url": "./static/media/omar-flores-lQT_bOWtysE-unsplash.e687fa09.jpg"
+  }
+]);
